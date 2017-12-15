4ff47d2fbf707ea7a8e4102579adc4ac085f9aad
Sivaraj Velayutham <34560264+sivarajvelayutham27@users.noreply.github.com>
2017-12-15 00:20:02 -0500

    Decision making examples
    If loop examples

---
+++ DecisionMakingAndLoops/src/com/sv/java/DecisionMaking.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.sv.java;
+
+/**
+ * @author Sivaraj Velayutham
+ *
+ */
+public class DecisionMaking {
+	
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 4;
+		
+		if(a == b) {
+			System.out.println("a and b are equal ");
+		}else {
+			System.out.println("a and b are not equal");
+		}
+		
+	}
+
+}
